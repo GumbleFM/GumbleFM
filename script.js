2d8f5aabc7ed6a8b97e6133eff111dbca353179f
GumbleFM <johnygumble@gmail.com>
2026-07-12 16:09:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -32,36 +32,3 @@ radio.addEventListener("pause", () => {
     if (playBtn) playBtn.textContent = "▶ Ouvir Agora";
 });
 
-/* ---------- BOTÃO PLAY ---------- */
-
-.controls{
-display:flex;
-align-items:center;
-gap:25px;
-margin-top:20px;
-}
-
-#playBtn{
-min-width:220px;
-height:64px;
-border:none;
-border-radius:50px;
-background:#1DB954;
-color:#fff;
-font-size:18px;
-font-weight:700;
-cursor:pointer;
-transition:.25s;
-box-shadow:0 0 25px rgba(29,185,84,.35);
-}
-
-#playBtn:hover{
-transform:translateY(-2px) scale(1.03);
-box-shadow:0 0 40px rgba(29,185,84,.55);
-}
-
-#volume{
-width:180px;
-accent-color:#1DB954;
-cursor:pointer;
-}
