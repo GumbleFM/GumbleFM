35cb8b25f32a197f9e99126578991ae5f879f9b4
GumbleFM <johnygumble@gmail.com>
2026-07-13 09:45:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const radio = document.getElementById("radio");
 const playBtn = document.getElementById("playBtn");
 const volume = document.getElementById("volume");
 
+const songTitle = document.getElementById("songTitle");
+const songArtist = document.getElementById("songArtist");
+
 playBtn.onclick = function () {
 
     if (radio.paused) {
@@ -21,3 +24,31 @@ playBtn.onclick = function () {
 volume.oninput = function () {
     radio.volume = this.value;
 };
+
+async function updateSong() {
+
+    try {
+
+        const response = await fetch("https://uk3freenew.listen2myradio.com:20795/currentsong?sid=1");
+
+        const text = await response.text();
+
+        if (text.trim() !== "") {
+
+            songTitle.textContent = "🎵 " + text;
+            songArtist.textContent = "🔴 Em direto";
+
+        }
+
+    } catch {
+
+        songTitle.textContent = "GumbleFM";
+        songArtist.textContent = "🔴 Em direto";
+
+    }
+
+}
+
+updateSong();
+
+setInterval(updateSong, 5000);
